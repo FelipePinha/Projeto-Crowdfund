46d7a517804bac4a46069f88a93b00fddeb2146c
Felipe Pinha <felipe_pc7@hotmail.com>
2022-10-27 20:10:53 -0300

    style adjustments

--- src/components/BacksData/InfoCard/InfoCard.styles.js
+++ src/components/BacksData/InfoCard/InfoCard.styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const CardStyles = styled.section`
-    width: 40%;
+    width: 45%;
     min-width: 340px;
     background-color: #fff;
     box-shadow: 0 0 1px rgba(0, 0, 0, .2);
@@ -11,10 +11,10 @@ export const CardStyles = styled.section`
     flex-direction: column;
     justify-content: center;
     row-gap: 25px;
-    margin: 190px auto;
+    margin: 190px auto auto auto;
     padding: 25px;
 
     @media (max-width: 768px) {
-        margin: 290px auto;
+        margin: 290px auto auto auto;
     }
 `
--- src/components/UpperCard/TopCard/TopCard.styles.js
+++ src/components/UpperCard/TopCard/TopCard.styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Card = styled.section`
-    width: 40%;
+    width: 45%;
     min-width: 340px;
     background-color: #fff;
     box-shadow: 0 0 1px rgba(0, 0, 0, .2);
